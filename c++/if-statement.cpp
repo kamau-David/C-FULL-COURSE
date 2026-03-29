65cdd0e84b237b555d12999063aa873e30617977
kamau-David <dayvieek@gmail.com>
2026-03-29 23:09:32 +0300

    if conditions

---
+++ c++/if-statement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age =17;
+
+    if(age >= 18){
+        cout<<"you are an adult";
+    }else if(age>=12){
+        cout<<"You are a teenager";
+    }
+    else{
+        cout<<"you are a child";
+    }
+
+}
