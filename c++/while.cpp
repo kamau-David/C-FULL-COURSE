11c42866e4062713e88dbc32523d600c0d7e70be
kamau-David <dayvieek@gmail.com>
2026-03-29 22:53:13 +0300

    while loop

---
+++ c++/while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number =1;
+
+    while (number <=10)
+    {
+    cout<<"hello world"<<endl;
+    number = number + 1;
+}
+    return 0;
+}
