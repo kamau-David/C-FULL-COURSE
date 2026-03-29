8ac8c9bd1f3ee677b60f02093e28cbab9db3f483
kamau-David <dayvieek@gmail.com>
2026-03-29 23:12:30 +0300

    while loop

---
+++ c++/while-loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int counter =1;
+    while (counter<=5){
+        cout<<"count is "<<counter<<endl;
+        counter++;
+        
+    }
+    return 0;
+}
